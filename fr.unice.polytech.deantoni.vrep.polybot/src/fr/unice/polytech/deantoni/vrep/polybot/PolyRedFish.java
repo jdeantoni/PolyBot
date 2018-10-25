c81fc458654aae870fb7ca1c2107ee0dd8cbaeb9
jdeanton <julien.deantoni@inria.fr>
2018-10-25 18:23:09 +0200

    minor changes

--- fr.unice.polytech.deantoni.vrep.polybot/src/fr/unice/polytech/deantoni/vrep/polybot/PolyRedFish.java
+++ fr.unice.polytech.deantoni.vrep.polybot/src/fr/unice/polytech/deantoni/vrep/polybot/PolyRedFish.java
@@ -1,4 +1,5 @@
 package fr.unice.polytech.deantoni.vrep.polybot;
+
 import java.util.ArrayList;
 
 import fr.unice.polytech.deantoni.vrep.polybot.robot.PolyRob;
@@ -6,60 +7,59 @@ import fr.unice.polytech.deantoni.vrep.polybot.utils.Blob;
 import fr.unice.polytech.deantoni.vrep.polybot.utils.Position2D;
 
 public class PolyRedFish {
-	
+
 	public static void main(String[] args) {
 		PolyRob rob = new PolyRob("127.0.0.1", 19997);
 		redFishBehaviour(rob);
-		//dumbRedFishStuff(rob);
+		// dumbRedFishStuff(rob);
 	}
 
 	protected static void redFishBehaviour(PolyRob rob) {
 		rob.start();
 		rob.sleep(800);
 		int i = 0;
-		while(i < 5) {
+		while (i < 5) {
 			i++;
-			 double dist = 100;
+			double dist = 100;
 			while (!rob.hasDetectedAnObject() || dist > 75) {
-				 dist = rob.getDetectedObjectDistance();
+				dist = rob.getDetectedObjectDistance();
 				ArrayList<Blob> blobs = rob.getViewableBlobs();
-				for(Blob b : blobs) {
-					System.out.println("\t blob ["+b.positionX+" ; "+b.positionY+"]");
+				for (Blob b : blobs) {
+					System.out.println("\t blob [" + b.positionX + " ; " + b.positionY + "]");
 				}
 				Position2D pos = rob.getPosition();
-				System.out.println("rob ["+pos.x+" ; "+pos.y+"]");
-				int sl = (int) (Math.random()*15);
-				int sr = (int) (Math.random()*15);
+				System.out.println("rob [" + pos.x + " ; " + pos.y + "]");
+				int sl = (int) (Math.random() * 15);
+				int sr = (int) (Math.random() * 15);
 				rob.goCurved(sl, sr);
 				rob.sleep(100);
 			}
-			
-		
-			
-			
-		   rob.goStraight(0);
-	       dist = rob.getDetectedObjectDistance();
-	       rob.log2vrep("dist "+dist);
-	
-			while(dist > 55) {
-				
-				System.out.println(dist+"\n\t"+rob.detectedObjectPoint.getArray()[0]+" "+rob.detectedObjectPoint.getArray()[1]+" "+rob.detectedObjectPoint.getArray()[2]);
+
+			rob.goStraight(0);
+			dist = rob.getDetectedObjectDistance();
+			rob.log2vrep("dist " + dist);
+
+			while (dist > 55) {
+
+				System.out.println(dist + "\n\t" + rob.detectedObjectPoint.getArray()[0] + " "
+						+ rob.detectedObjectPoint.getArray()[1] + " " + rob.detectedObjectPoint.getArray()[2]);
 				rob.goStraight(2);
-				if (rob.detectedObjectPoint.getArray()[0] > 0){
-					rob.goCurved(2,3);
+				if (rob.detectedObjectPoint.getArray()[0] > 0) {
+					rob.goCurved(2, 3);
 					System.out.println("turn left");
-				}
-				else{
-					rob.goCurved(3,2);
+				} else {
+					rob.goCurved(3, 2);
 					System.out.println("turn right");
 				}
-				rob.log2vrep("dist "+dist);
+				rob.log2vrep("dist " + dist);
 				rob.sleep(100);
 				rob.hasDetectedAnObject();
-				dist = rob.detectedObjectPoint.getArray()[0]*rob.detectedObjectPoint.getArray()[0]+rob.detectedObjectPoint.getArray()[1]*rob.detectedObjectPoint.getArray()[1]+rob.detectedObjectPoint.getArray()[2]*rob.detectedObjectPoint.getArray()[2];
+				dist = rob.detectedObjectPoint.getArray()[0] * rob.detectedObjectPoint.getArray()[0]
+						+ rob.detectedObjectPoint.getArray()[1] * rob.detectedObjectPoint.getArray()[1]
+								+ rob.detectedObjectPoint.getArray()[2] * rob.detectedObjectPoint.getArray()[2];
 			}
-			rob.log2vrep("last dist "+dist);
-			
+			rob.log2vrep("last dist " + dist);
+
 			rob.closeGrip();
 			rob.sleep(1500);
 			rob.goStraight(-4, 500);
@@ -68,7 +68,7 @@ public class PolyRedFish {
 			rob.sleep(4000);
 			rob.openGrip();
 			rob.goStraight(-5, 3000);
-			rob.turnLeft(6,1000);
+			rob.turnLeft(6, 1000);
 			rob.sleep(1500);
 		}
 		rob.stopSimulation();
@@ -77,10 +77,10 @@ public class PolyRedFish {
 	protected static void dumbRedFishStuff(PolyRob rob) {
 		int i = 0;
 		rob.start();
-		while(i++ < 10) {
-			if(i%2 == 0) {
+		while (i++ < 10) {
+			if (i % 2 == 0) {
 				rob.openGrip();
-			}else {
+			} else {
 				rob.closeGrip();
 			}
 			rob.goStraight(5);
@@ -89,10 +89,10 @@ public class PolyRedFish {
 			}
 			rob.goStraight(-6);
 			rob.sleep(400);
-			if(rob.detectedObjectPoint.getArray()[2] > rob.detectedObjectPoint.getArray()[0]) {
+			if (rob.detectedObjectPoint.getArray()[2] > rob.detectedObjectPoint.getArray()[0]) {
 				rob.turnRight(6);
 				rob.sleep(600);
-			}else {
+			} else {
 				rob.turnRight(6);
 				rob.sleep(600);
 			}
